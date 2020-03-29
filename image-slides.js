1a5b52550604b5475af9bcef61f7d092371dadca
Milskaly <54206186+Milskaly@users.noreply.github.com>
2020-03-29 22:49:27 +0100

    Delete image-slides.js

--- image-slides.js
+++
@@ -1,9 +0,0 @@
-let slides = document.querySelectorAll(".slides .slide");
-let currentSlide = 0;
-let slideInterval = setInterval(nextSlide, 2500);
-
-function nextSlide() {
-	slides[currentSlide].className = "slide";
-	currentSlide = (currentSlide + 1) % slides.length;
-	slides[currentSlide].className = "slide showing";
-}
