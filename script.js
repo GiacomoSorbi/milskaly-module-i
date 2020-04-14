afd7aa4ecdb0bc8b5e8605de8707ae899f33f8d8
Emilia Kalyvides <emiliakalyvides@Emilias-iMac.connect>
2020-04-14 13:46:10 +0100

    changes to shop page

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ if (document.readyState == "loading") {
 // activate remove buttons
 // add an event to say when the button is clicked the item should be removed
 function ready() {
-	let removeCartButtonItems = document.getElementsByClassName(
+	let removeBasketButtonItems = document.getElementsByClassName(
 		"button-purchase"
 	);
-	for (let i = 0; i < removeCartButtonItems.length; i++) {
-		let button = removeCartButtonItems[i];
-		button.addEventListener("click", removeCartItem);
+	for (let i = 0; i < removeBasketButtonItems.length; i++) {
+		let button = removeBasketButtonItems[i];
+		button.addEventListener("click", removeBasketItem);
 	}
 
 	// event listener to determine any time the quanity input changes its value
@@ -23,11 +23,11 @@ function ready() {
 		let input = quantityInputs[i];
 		input.addEventListener("change", quantityChanged);
 	}
-	// add to cart buttons
-	let addToCartButtons = document.getElementsByClassName("shop-item-button");
-	for (let i = 0; i < addToCartButtons.length; i++) {
-		let button = addToCartButtons[i];
-		button.addEventListener("click", addToCartClicked);
+	// add to Basket buttons
+	let addToBasketButtons = document.getElementsByClassName("shop-item-button");
+	for (let i = 0; i < addToBasketButtons.length; i++) {
+		let button = addToBasketButtons[i];
+		button.addEventListener("click", addToBasketClicked);
 
 		document
 			.getElementsByClassName("button-purchase")[0]
@@ -38,18 +38,18 @@ function ready() {
 // Creates an alert to confirm you've purchased an item
 function purchaseClicked() {
 	alert("Thank you for your purchase");
-	let cartItems = document.getElementsByClassName("basket-items")[0];
-	while (cartItems.hasChildNodes()) {
-		cartItems.removeChild(cartItems.firstChild);
+	let BasketItems = document.getElementsByClassName("basket-items")[0];
+	while (BasketItems.hasChildNodes()) {
+		BasketItems.removeChild(BasketItems.firstChild);
 	}
 
-	updateCartTotal();
+	updateBasketTotal();
 }
-//removes cart item
-function removeCartItem(event) {
+//removes Basket item
+function removeBasketItem(event) {
 	let buttonClicked = event.target;
 	buttonClicked.parentElement.parentElement.remove();
-	updateCartTotal();
+	updateBasketTotal();
 }
 // function for quantity input, checks input to see if it's a number
 function quantityChanged(event) {
@@ -57,67 +57,70 @@ function quantityChanged(event) {
 	if (isNaN(input.value) || input.value <= 0) {
 		input.value = 1;
 	}
-	updateCartTotal();
+	updateBasketTotal();
 }
-// adds item, images and price to cart by class name
-function addToCartClicked(event) {
+// adds item, images and price to Basket by class name
+function addToBasketClicked(event) {
 	let button = event.target;
 	let shopItem = button.parentElement.parentElement;
 	let title = shopItem.getElementsByClassName("product-title")[0].innerText;
 	let price = shopItem.getElementsByClassName("product-price")[0].innerText;
 	let imageSrc = shopItem.getElementsByClassName("product-image")[0].src;
-	addItemToCart(title, price, imageSrc);
-	updateCartTotal();
+	addItemToBasket(title, price, imageSrc);
+	updateBasketTotal();
 }
 
-//creates cart row/element for item to cart items
-function addItemToCart(title, price, imageSrc) {
-	let cartRow = document.createElement("div");
-	cartRow.classList.add("basket-row");
-	let cartItems = document.getElementsByClassName("basket-items")[0];
-	let cartItemNames = cartItems.getElementsByClassName("basket-item-title");
+//creates Basket row/element for item to Basket items
+function addItemToBasket(title, price, imageSrc) {
+	let BasketRow = document.createElement("div");
+	BasketRow.classList.add("basket-row");
+	let BasketItems = document.getElementsByClassName("basket-items")[0];
+	let BasketItemNames = BasketItems.getElementsByClassName("basket-item-title");
 	//stops items in basket duplicating
-	for (let i = 0; i < cartItemNames.length; i++) {
-		if (cartItemNames[i].innerText == title) {
-			alert("This item is already in your cart");
+	for (let i = 0; i < BasketItemNames.length; i++) {
+		if (BasketItemNames[i].innerText == title) {
+			alert("This item is already in your basket");
 			return;
 		}
 	}
-	let cartRowContents = `
-	<div class="basket-item cart-column">
+	let BasketRowContents = `
+	<div class="basket-item">
 	<img class="basket-item-image" src="${imageSrc}">
-	<span class="basket-item-title"><p>${title}</p></span>
+	<div class="basket-item-title"><p>${title}</p></div>
 </div>
-<span class="basket-price cart-column"><p>${price}</p> </span>
-<div class="basket-quantity cart-column">
+<div class="basket-price"><p>${price}</p> </div>
+<div class="basket-quantity">
 	<input class="basket-quantity-input" type="number" value="1">
-	<button class="button button-purchase" type="button">REMOVE</button>
+	<button class="button button-remove fa fa-trash" type="button"></button>
 </div>
 </div>`;
-	cartRow.innerHTML = cartRowContents;
-	cartItems.append(cartRow);
 
-	//removes cart items after the remove button has been clicked
-	cartRow
-		.getElementsByClassName("button-purchase")[0]
-		.addEventListener("click", removeCartItem);
+	BasketRow.innerHTML = BasketRowContents;
+	BasketItems.append(BasketRow);
 
-	cartRow
-		.getElementsByClassName("basket-quantity-input")[0]
-		.addEventListener("change", quantityChanged);
+	//removes Basket items after the remove button has been clicked
+	BasketRow.getElementsByClassName("button-remove")[0].addEventListener(
+		"click",
+		removeBasketItem
+	);
+
+	BasketRow.getElementsByClassName("basket-quantity-input")[0].addEventListener(
+		"change",
+		quantityChanged
+	);
 }
 
-// Update cart total
+// Update Basket total
 // Find the price of all items in each row and the quanity and multiply them
 // Add together to get the total
-function updateCartTotal() {
-	let cartItemContainer = document.getElementsByClassName("basket-items")[0];
-	let cartRows = cartItemContainer.getElementsByClassName("basket-row");
+function updateBasketTotal() {
+	let BasketItemContainer = document.getElementsByClassName("basket-items")[0];
+	let BasketRows = BasketItemContainer.getElementsByClassName("basket-row");
 	let total = 0;
-	for (let i = 0; i < cartRows.length; i++) {
-		let cartRow = cartRows[i];
-		let priceElement = cartRow.getElementsByClassName("basket-price")[0];
-		let quantityElement = cartRow.getElementsByClassName(
+	for (let i = 0; i < BasketRows.length; i++) {
+		let BasketRow = BasketRows[i];
+		let priceElement = BasketRow.getElementsByClassName("basket-price")[0];
+		let quantityElement = BasketRow.getElementsByClassName(
 			"basket-quantity-input"
 		)[0];
 		let price = parseFloat(priceElement.innerText.replace("£", ""));
