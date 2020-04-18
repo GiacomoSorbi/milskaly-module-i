c177bd256dc1ff2aeb0b22878e6a2e7405aa3b18
Emilia Kalyvides <emiliakalyvides@Emilias-iMac.local>
2020-04-18 21:46:42 +0100

    general updates

---
+++ Read-more.js
@@ -0,0 +1,8 @@
+function viewOlderPosts() {
+	x = document.getElementById("blog-row-two");
+	if (x.style.display === "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "block";
+	}
+}
